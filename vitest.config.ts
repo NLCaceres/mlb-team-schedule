7b9d9b5fbddddd576defe1edc399bd9c8b6b55ca
NLCaceres <NLCaceres21@Gmail.com>
2023-07-07 00:11:57 -0700

    Update Vitest Config to test Typescript Components

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vitest/config'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vitest/config"
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 export default defineConfig({
   plugins: [
-    svelte({ hot: !process.env.VITEST }),
+    svelte({ hot: !process.env.VITEST,  preprocess: vitePreprocess() })
   ],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: 'client/setupTest.ts'
+    environment: "jsdom",
+    setupFiles: "client/setupTest.ts"
   },
 })
