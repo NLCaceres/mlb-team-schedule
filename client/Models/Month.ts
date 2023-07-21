2441eeaa6a919968c6ebe3c00255204d25e86fd9
NLCaceres <NLCaceres21@Gmail.com>
2023-07-21 14:00:28 -0700

    Drop TS enum usage + unneeded Month interface

--- client/Models/Month.ts
+++ client/Models/Month.ts
@@ -1,19 +1,3 @@
-export enum Day {
-  Sunday,
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday
-}
-
-export interface Month {
-  monthName: string,
-  startDay: Day,
-  numDays: number,
-}
-
 export const MONTH_MAP = {
   "January": 1,
   "February": 2,
