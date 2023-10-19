063ae4b62b2c6690a0f34b93f888722111678b28
NLCaceres <NLCaceres21@Gmail.com>
2023-10-18 23:55:51 -0700

    Update available & useful endpoints for MLB Stats API

--- utility/endpoint_helpers.py
+++ utility/endpoint_helpers.py
@@ -1,36 +1,41 @@
 
 #! Constants
-#? https://bit.ly/2XbrNVn - URL to Github MLB-stats python-based wrapper
-#? Changing teamId in this URL changes homeTeam to desired one
+#? https://github.com/toddrob99/MLB-StatsAPI/wiki
+#todo A variable 'teamId' in this URL can be used to change the homeTeam to a desired one
 SCHEDULE_ENDPOINT = ('https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&hydrate=team' #? Concatenated on compile
                     ',game(promotions)&season={seasonYear}&startDate={startDate}&endDate={endDate}' 
                     '&teamId=119&gameType=R,F,D,L,W&scheduleTypes=games')
 #? This URL grabs the latest game so it can be useful for grabbing a Team's win-loss record
 LATEST_GAME_URL = 'https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&teamId={espnID}'
-#TODO Double check above seemingly grabs latest game even if day has past, and even if actual current day is an off day
+#? Above URL grabs the most recent game up until 6-8AM PST the next day
+#? BUT on off-days, by 8AM the last game will disappear, and no games will be returned
 
-#? More examples of various endpoints
-ALL_GAMES_OF_THE_DAY_URL = 'https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1' #? Grabs all games of the day
-ALL_MLB_TEAMS_URL = 'https://statsapi.mlb.com/api/v1/teams?lang=en&sportId=1' #? Grabs all teams
+#! More useful endpoints
+#? Insert the team's ID into this URL to grab their logo as an SVG
+BASE_MLB_LOGO_URL = 'https://www.mlbstatic.com/team-logos/{espnID}.svg'
+#? Grabs all games of the day
+ALL_GAMES_OF_THE_DAY_URL = 'https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1'
+#? Grabs all teams
+ALL_MLB_TEAMS_URL = 'https://statsapi.mlb.com/api/v1/teams?lang=en&sportId=1'
 #? Grabs all National League teams -> 203 - 205 NL West, East, Central with Records
 NL_TEAMS_URL = 'https://statsapi.mlb.com/api/v1/standings?leagueId=104'
 #? Grabs all American League teams -> 200 - 202 AL West, East, Central with Records
 AL_TEAMS_URL = 'https://statsapi.mlb.com/api/v1/standings?leagueId=103'
 #? Grabs BOTH the NL and AL in a group of 6 items (0-5) listing off teams in Standings Order
 LEAGUE_STANDINGS_URL = 'https://statsapi.mlb.com/api/v1/standings?leagueId=103&leagueId=104'
-#? This URL helps for grabbing info on certain meta/key/urlParam types - https://bit.ly/3CLOyjl known types
-META_KEYS_URL = 'https://statsapi.mlb.com/api/v1/gameTypes'
-#* This particular meta_key url grabs info on gameTypes, notably:
-  #* 'F' = 'Wild Card', 'D' = 'Division Series', 'L' = 'League Series', 'W' = 'World Series', and 'A' = 'All Star Game'
+
+#? Helpful URL to grab info on known meta/key/urlParam types - https://github.com/toddrob99/MLB-StatsAPI/wiki/Function:-meta
+GAME_TYPE_META_KEYS_URL = 'https://statsapi.mlb.com/api/v1/gameTypes'
+#* Meta Tag Definition: 'A' = 'All Star Game'
 ALL_STAR_GAME_URL = 'https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&season=2023&gameType=A'
-#? Seems as of 2023, there isn't any way of getting Playoff dates until the teams are finalized in late September
-#? Additionally, as of 2023, the World Series Game 7 CAN possibly occur as late as November 4th
-#* A good possible URL for the playoffs would be 'https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&season=2023&gameType=P'
+#* Meta Tag Definitions: 'F' = 'Wild Card', 'D' = 'Division Series', 'L' = 'League Series', 'W' = 'World Series'
+#* 'P' = 'Playoff' BUT it seems to be some kind of legacy meta-tag that is no longer used
+#? Seems as of 2023, Playoff dates aren't available until the teams are finalized in late Sept or early October
+#? With the playoffs POSSIBLY lasting until a November 4th (in 2023) World Series Game 7
+PLAYOFFS_URL = 'https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&season=2023&gameType=F,D,L,W'
 
 #? Following is a concrete example that grabs all Dodgers games in 2023
 # https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&hydrate=team,game(promotions)&season=2023&startDate=2023-03-01&endDate=2023-10-31&teamId=119&gameType=R,F,D,L,W&scheduleTypes=games
 DODGER_GAMES_2023_URL = ('https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&hydrate=team'
                         ',game(promotions)&season=2023&startDate=2023-03-01&endDate=2023-10-31'
                         '&teamId=119&gameType=R,F,D,L,W&scheduleTypes=games')
-
-BASE_MLB_LOGO_URL = 'https://www.mlbstatic.com/team-logos/{espnID}.svg'
