8c63491ec51d2b303268c26f8649704822537027
NLCaceres <NLCaceres21@Gmail.com>
2023-10-25 23:49:06 -0700

    Make a few simple, reusable, & descriptive assertion methods

---
+++ tests/common_assertions.py
@@ -0,0 +1,13 @@
+#* Common and helpful assertion methods a la Vitest, Jest, or JUnit
+
+def assertIsNone(someVar):
+    assert someVar is None
+
+def assertIsNotNone(someVar):
+    assert someVar is not None
+
+def assertIsEmpty(someSequence):
+    assert len(someSequence) == 0
+
+def assertHasLengthOf(someSequence, length):
+    assert len(someSequence) == length
