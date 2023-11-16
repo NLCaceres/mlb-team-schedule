0012e3a4c8f58a1c048001b81056101de08df055
NLCaceres <NLCaceres21@Gmail.com>
2023-11-16 00:26:11 -0800

    Create Flask API Error handlers + add abort() triggers

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, url_for, jsonify
+from flask import Blueprint, abort, jsonify, request, redirect, url_for
 from calendar import monthrange
 from datetime import date
 from . import db
@@ -31,7 +31,7 @@ def apiSingleMonthSchedule(month):
     try:
         monthNum = MONTH_SWITCH[month]
     except KeyError:
-        return { 'message': 'Invalid Month!' }
+        abort(404, 'Invalid month')
 
     year = date.today().year
     start = date(year=year, month=monthNum, day=1)
@@ -53,12 +53,12 @@ def apiSingleDaySchedule(month, day):
     try:
         monthNum = MONTH_SWITCH[month]
     except KeyError:
-        return { 'message': 'Invalid Month!' } #TODO Return status codes!
+        abort(404, 'Invalid month')
 
     year = date.today().year
     lastDayOfMonth = monthrange(year=year, month=MONTH_SWITCH[month])[1] #? Returns tuple (firstDay, lastDay)
     if (day <= 0 or day > lastDayOfMonth):
-        return { 'message': 'Invalid Month!' }
+        abort(404, 'Invalid day of the month')
 
     start = date(year=year, month=monthNum, day=day)
     endMonth = monthNum if (day != lastDayOfMonth) else monthNum + 1
@@ -69,3 +69,25 @@ def apiSingleDaySchedule(month, day):
         db.select(BaseballGame).where(BaseballGame.readableDateTime < end).where(BaseballGame.readableDateTime >= start)
     ) #? Since scalars() returns an iterable ScalarResult, only need to suffix .all() if a basic List is explicitly needed
     return jsonify([game.asDict for game in baseballGames])
+
+
+#? Flask Exceptions generally descend from this Werkzeug base Exception so it's super easy for simple cases to use
+# @bp.errorhandler(HTTPException) #? BUT, of course, this is like catching Exception, you MIGHT catch more than expected
+# def genericExceptionHandler(e: HTTPException):
+    #? Returning a tuple means sending (response, status_code) OR (response, headers) depending on types used
+    #? The response will have make_response called on it which ONLY calls jsonify() if a dict or list is found
+    # return jsonify(error=str(e)), e.code #? KeyArgs in jsonify() become key:val pairs in the response's JSON Body
+
+    # return str(e), 404 #? In this case, Javascript calls to response.json() will fail, so use response.text() instead
+
+    #? Sending a redirect to the API from the Flask backend sends back a Template! So better to craft a response like below
+    # response = make_response(jsonify(navTo='/fullSchedule'))
+    # response.status_code = 302
+    # return response
+
+#? Regardless of order declared, the MOST SPECIFIC error handler Flask has registered will be used
+#? Ex: IF abort(404) is run, THEN this handler catches it, NOT the above generic handler,
+#? BUT IF any other exceptions were raised, THEN they'd slip through, and Flask would run `abort(500)` by default instead
+@bp.errorhandler(404)
+def notFound(e):
+    return jsonify(error=str(e)), 404
