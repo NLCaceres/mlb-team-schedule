39d5641ebff7b69690dc3af62e305f63ac69c8c7
NLCaceres <NLCaceres21@Gmail.com>
2023-11-16 01:15:45 -0800

    Use ApiError class to handle unexpected HTTP status codes

--- client/API/utility.test.ts
+++ client/API/utility.test.ts
@@ -1,29 +1,79 @@
-import getRequest from "./utility";
+import getRequest, { ApiError } from "./utility";
 import { vi } from "vitest";
 import type { Mock } from "vitest";
 
-const fetchMockHelp = (jsonResponse: string, statusCode: number = 200) => 
+type errorJson = { error: string }
+type invalidErrorJson = { errMsg: string }
+const createMockResponse = (jsonResponse: string | errorJson | invalidErrorJson, statusCode: number = 200) => 
   vi.fn(() => Promise.resolve({ json: () => Promise.resolve(jsonResponse), status: statusCode })) as Mock;
 
 describe("like a 'fetch' wrapper", () => {
   test("returns the json response", async () => {
-    const fetchSpy = vi.spyOn(global, "fetch").mockImplementationOnce(fetchMockHelp("Fizz"));
+    const fetchSpy = vi.spyOn(global, "fetch").mockImplementationOnce(createMockResponse("Fizz"));
 
     const response = await getRequest("foobar");
     expect(response).toBe("Fizz");
-    expect(fetchSpy).toHaveBeenCalledWith("foobar", { headers: {"Accept": "application/json"} });
+    expect(fetchSpy).toHaveBeenCalledWith("foobar", { headers: { "Accept": "application/json" } });
   })
-  test("returns undefined if a 400+ status code or an empty response", async () => {
-    const fetchSpy = vi.spyOn(global, "fetch").mockImplementationOnce(fetchMockHelp("Fizz", 400));
+  describe("an error is thrown if an unexpected status code is found", () => {
+    test("like a 500-599 status code", async () => {
+      const fetchSpy = vi.spyOn(global, "fetch").mockImplementationOnce(createMockResponse("Fizz", 500));
 
-    const badResponse = await getRequest("foobar");
-    expect(badResponse).toBe(undefined);
-    expect(fetchSpy).toHaveBeenCalledWith("foobar", { headers: {"Accept": "application/json"} });
+      //? To test for errors thrown in async funcs, must wrap the func in a closure AND use `rejects` to catch the promise
+      await expect(() => getRequest("foobar")).rejects.toThrowError(ApiError); //? SO can test for general type matching
+      expect(fetchSpy).toHaveBeenCalledWith("foobar", { headers: { "Accept": "application/json" } });
 
-    fetchSpy.mockImplementationOnce(fetchMockHelp(""));
+      fetchSpy.mockImplementationOnce(createMockResponse({ error: "500 Internal Server Error" }, 500));
+      await expect(() => getRequest("foobar")).rejects.toThrowError("500 Internal Server Error"); //? OR to test for specific error messaging
+      expect(fetchSpy).toHaveBeenCalledWith("foobar", { headers: { "Accept": "application/json" } });
+
+      expect(fetchSpy).toHaveBeenCalledTimes(2);
+    })
+    test("like a 400-499 status code", async () => {
+      const fetchSpy = vi.spyOn(global, "fetch").mockImplementationOnce(createMockResponse("Fizz", 400));
+
+      //? Can also test for a very specific concrete error
+      await expect(() => getRequest("foobar")).rejects.toThrowError(new ApiError({ name: "CLIENT_ERROR_STATUS", message: "" }));
+      expect(fetchSpy).toHaveBeenCalledWith("foobar", { headers: { "Accept": "application/json" } });
+      
+      //* WHEN error json found with an error key, THEN it is used as the ApiError message
+      fetchSpy.mockImplementationOnce(createMockResponse({ error: "404 Not Found" }, 404));
+      await expect(() => getRequest("foobar")).rejects.toThrowError("404 Not Found");
+
+      //* WHEN no error json is found, THEN the error message will be empty (albeit still an ApiError)
+      fetchSpy.mockImplementationOnce(createMockResponse("", 400));
+      await expect(() => getRequest("foobar")).rejects.toThrowError("");
+      
+      fetchSpy.mockImplementationOnce(createMockResponse("", 400));
+      await expect(() => getRequest("foobar")).rejects.toThrowError(ApiError);
+
+      expect(fetchSpy).toHaveBeenCalledTimes(4);
+    })
+    test("like a 300-399 status code", async () => {
+      const fetchSpy = vi.spyOn(global, "fetch").mockImplementationOnce(createMockResponse("Fizz", 302));
+      //* WHEN no error key-val pair in the errorJson is found, no message is inserted into the ApiError
+      await expect(() => getRequest("foobar")).rejects.toThrowError(new ApiError({ name: "REDIRECT_STATUS", message: "" }));
+
+      //* WHEN an unexpected error key-val pair in the errorJson is found, no message is inserted into the ApiError
+      fetchSpy.mockImplementationOnce(createMockResponse({ errMsg: "301 Moved Permanently" }, 301));
+      await expect(() => getRequest("foobar")).rejects.toThrowError(new ApiError({ name: "REDIRECT_STATUS", message: "" }));
+
+      //* WHEN the CORRECT error key-val pair in the errorJson is found, its message is inserted into the ApiError
+      fetchSpy.mockImplementationOnce(createMockResponse({ error: "301 Moved Permanently" }, 301));
+      await expect(() => getRequest("foobar")).rejects.toThrowError("301 Moved Permanently");
+
+      fetchSpy.mockImplementationOnce(createMockResponse({ error: "302 Found" }, 302));
+      await expect(() => getRequest("foobar")).rejects.toThrowError(new ApiError({ name: "REDIRECT_STATUS", message: "302 Found" }));
+
+      expect(fetchSpy).toHaveBeenCalledTimes(4);
+    })
+  })
+  test("returns undefined if an empty response is found", async () => {
+    const fetchSpy = vi.spyOn(global, "fetch").mockImplementationOnce(createMockResponse(""));
+
     const otherBadResponse = await getRequest("foobar");
     expect(otherBadResponse).toBe(undefined);
-    expect(fetchSpy).toHaveBeenCalledWith("foobar", { headers: {"Accept": "application/json"} });
-    expect(fetchSpy).toHaveBeenCalledTimes(2);
+    expect(fetchSpy).toHaveBeenCalledWith("foobar", { headers: { "Accept": "application/json" } });
+    expect(fetchSpy).toHaveBeenCalledTimes(1);
   })
 })
--- client/API/utility.ts
+++ client/API/utility.ts
@@ -6,14 +6,18 @@ const DEFAULT_HEADERS = { headers: {"Accept": "application/json"} };
 
 export default async function getRequest(endpointURL: string) {
   const response  = await fetch(endpointURL, DEFAULT_HEADERS);
-  if (response.status >= 400) { return undefined }
-
+  //? `response.text()` can be a helpful alternative if json() parsing the Response's ReadableStream body were to fail
   const jsonResponse = await response.json(); //? Await needed! Or else, no parsing happens
-  if (jsonResponse.length === 0) { return undefined }
+
+  if (response.status >= 500) { throw new ApiError({ name: "SERVER_ERROR_STATUS", message: jsonResponse?.error ?? "" }) }
+  else if (response.status >= 400) { throw new ApiError({ name: "CLIENT_ERROR_STATUS", message: jsonResponse?.error ?? "" }) }
+  else if (response.status >= 300) { throw new ApiError({ name: "REDIRECT_STATUS", message: jsonResponse?.error ?? "" }) }
+
+  if (jsonResponse.length === 0) { return undefined } //todo Could fail if Json Response is just a plain object, and not an array
 
   return jsonResponse;
 }
 
 //* Typical Errors that may need to be thrown, related to the status codes the API may send instead of a 200 OK Response
-type ErrorNames = 'CLIENT_ERROR_STATUS' | 'SERVER_ERROR_STATUS' | 'REDIRECT_STATUS'
+type ErrorNames = "CLIENT_ERROR_STATUS" | "SERVER_ERROR_STATUS" | "REDIRECT_STATUS"
 export class ApiError extends ErrorBase<ErrorNames> { }
