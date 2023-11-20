309f18916b11c603cd9d4e2e4846c431cabbacc0
NLCaceres <NLCaceres21@Gmail.com>
2023-11-19 22:19:08 -0800

    Models implement typeAliases & add BaseballGame builder + computed prop

---
+++ client/Models/DataClasses.test.ts
@@ -0,0 +1,74 @@
+import BaseballGame, { Promotion } from "./DataClasses";
+
+
+describe("forms the main models of the app like", () => {
+  describe("a BaseballGame", () => {
+    test("with an optional static builder", () => {
+      const id = "1";
+      const date = "";
+      const homeTeam = { id: "1", teamLogo: "", teamName: "foo", cityName: "fizz", abbreviation: "", wins: 1, losses: 0 };
+      const awayTeam = { id: "1", teamLogo: "", teamName: "bar", cityName: "buzz", abbreviation: "", wins: 0, losses: 1 };
+      const promos: Promotion[] = [];
+      const seriesGameNumber = 1;
+      const seriesGameCount = 2;
+
+      const normalGame = new BaseballGame(id, date, homeTeam, awayTeam, promos, seriesGameNumber, seriesGameCount);
+      const builderGame = BaseballGame.of({ id, date, homeTeam, awayTeam, promos, seriesGameNumber, seriesGameCount });
+
+      //* WHEN using the builder, THEN it should completely match usage of the constructor
+      expect(builderGame.id).toBe(normalGame.id);
+      expect(builderGame.date).toBe(normalGame.date);
+      expect(builderGame.homeTeam).toBe(normalGame.homeTeam);
+      expect(builderGame.awayTeam).toBe(normalGame.awayTeam);
+      expect(builderGame.promos).toStrictEqual(normalGame.promos);
+      expect(builderGame.seriesGameNumber).toBe(normalGame.seriesGameNumber);
+      expect(builderGame.seriesGameCount).toBe(normalGame.seriesGameCount);
+
+      //* WHEN using the builder, THEN all values should be set to the values set in the build object
+      expect(builderGame.id).toBe("1");
+      expect(builderGame.date).toBe("");
+      expect(builderGame.homeTeam).toBe(homeTeam);
+      expect(builderGame.awayTeam).toBe(awayTeam);
+      expect(builderGame.promos).toStrictEqual([]);
+      expect(builderGame.seriesGameNumber).toBe(1);
+      expect(builderGame.seriesGameCount).toBe(2);
+    })
+    test("with a series description method", () => {
+      const homeTeam = { id: "1", teamLogo: "", teamName: "foo", cityName: "fizz", abbreviation: "", wins: 1, losses: 0 };
+      const awayTeam = { id: "1", teamLogo: "", teamName: "bar", cityName: "buzz", abbreviation: "", wins: 0, losses: 1 };
+      const game = BaseballGame.of({ id: "1", date: "", homeTeam, awayTeam, promos: [], seriesGameNumber: 1, seriesGameCount: 2 });
+
+      //* WHEN the seriesGameNumber == 1, THEN the series description should start with "The First Game"
+      expect(game.seriesDescription()).toContain("The First Game");
+      //* WHEN the seriesGameCount == 2, THEN the series description should end with "2-day Series"
+      expect(game.seriesDescription()).toContain("in a 2-day Series");
+
+      game.seriesGameNumber = 2;
+      //* WHEN the seriesGameNumber == seriesGameCount (BOTH 2 here), THEN the series description should start with "The Last Game"
+      expect(game.seriesDescription()).toContain("The Last Game");
+      expect(game.seriesDescription()).toContain("in a 2-day Series");
+
+      game.seriesGameCount = 3;
+      //* WHEN the seriesGameNumber == 2 BUT is not the last game, THEN the series description should use that number 2
+      expect(game.seriesDescription()).toContain("Game #2");
+      //* WHEN the seriesGameCount == 3, THEN the series description should end with "3-day Series"
+      expect(game.seriesDescription()).toContain("in a 3-day Series");
+
+      game.seriesGameNumber = 3;
+      //* WHEN the seriesGameNumber == seriesGameCount (BOTH 3 here), THEN the series description should start with "The Last Game"
+      expect(game.seriesDescription()).toContain("The Last Game");
+      expect(game.seriesDescription()).toContain("in a 3-day Series");
+
+      game.seriesGameCount = 4;
+      //* WHEN the seriesGameNumber == 3 BUT is not the last game, THEN the series description should use that number 3
+      expect(game.seriesDescription()).toContain("Game #3");
+      //* WHEN the seriesGameCount == 4, THEN the series description should end with "4-day Series"
+      expect(game.seriesDescription()).toContain("in a 4-day Series");
+
+      game.seriesGameNumber = 4;
+      //* WHEN the seriesGameNumber == seriesGameCount (BOTH 4 here), THEN the series description should start with "The Last Game"
+      expect(game.seriesDescription()).toContain("The Last Game");
+      expect(game.seriesDescription()).toContain("in a 4-day Series");
+    })
+  })
+})
--- client/Models/DataClasses.ts
+++ client/Models/DataClasses.ts
@@ -1,16 +1,44 @@
-export default class BaseballGame {
+//? Leverage TS types/interfaces as Class Property checkers (AND as a bonus for tests to create API mock JSON)
+export type BaseballGameProps = {
+  id: string, date: string,
+  homeTeam: BaseballTeam, awayTeam: BaseballTeam,
+  promos: Promotion[], seriesGameNumber: number, seriesGameCount: number
+}
+//? Implementing a TS type treats it like an interface
+export default class BaseballGame implements BaseballGameProps {
   constructor(public id: string, public date: string,
     public homeTeam: BaseballTeam, public awayTeam: BaseballTeam,
     public promos: Promotion[], public seriesGameNumber: number, public seriesGameCount: number) { }
+
+  //* Static Convenience Builder Func that replicates named args of Kotlin
+  static of({ id, date, homeTeam, awayTeam, promos, seriesGameNumber, seriesGameCount }: BaseballGameProps) {
+    return new BaseballGame(id, date, homeTeam, awayTeam, promos, seriesGameNumber, seriesGameCount);
+  }
+
+  //? JS can have weird behavior related to the `this` keyword where it'll unexpectedly change in the wrong context
+  //? An arrow func version of below would ensure expected usage BUT every instance would get its own using WAY MORE memory
+  //? TS provides `this` params to avoid the wasted memory, throwing if the context tries to change `this`
+  seriesDescription(this: BaseballGame) { //? `this` param will work like Python, getting erased at compile
+    const gameNumStr = (this.seriesGameNumber === 1) ? "The First Game" :
+      (this.seriesGameNumber === this.seriesGameCount) ? "The Last Game" : `Game #${this.seriesGameNumber}`;
+    return gameNumStr + ` in a ${this.seriesGameCount}-day Series`
+  }
 }
 
+export type BaseballTeamProps = {
+  id: string, teamLogo: string, teamName: string, cityName: string, abbreviation: string,
+  wins: number, losses: number, homeGames?: BaseballGameProps[], awayGames?: BaseballGameProps[]
+}
 export class BaseballTeam {
   constructor(public id: string, public teamLogo: string, public teamName: string,
     public cityName: string, public abbreviation: string, public wins: number, public losses: number,
-    public homeGames?: BaseballGame[], public awayGames?: BaseballGame[]) { }
+    public homeGames?: BaseballGameProps[], public awayGames?: BaseballGameProps[]) { }
 }
 
-export class Promotion {
+export type PromotionProps = {
+  id: string, name: string, thumbnailUrl: string, baseballGame?: BaseballGameProps
+}
+export class Promotion implements PromotionProps {
   constructor(public id: string, public name: string,
-    public thumbnailUrl: string, public baseballGame?: BaseballGame) { }
+    public thumbnailUrl: string, public baseballGame?: BaseballGameProps) { }
 }
