e209aa81afd7d3cbc133cbd2e5e160f1e8035654
NLCaceres <NLCaceres21@Gmail.com>
2023-11-25 17:53:27 -0800

    Fix `jest-dom` test setup for latest version of Vitest

--- client/setupTest.ts
+++ client/setupTest.ts
@@ -1,4 +1,1 @@
-import matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
-
-expect.extend(matchers); //* Add in Jest-Dom matchers to the normal ones
+import "@testing-library/jest-dom/vitest";
