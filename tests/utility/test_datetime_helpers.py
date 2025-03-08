16ae527a4e62fe3fb4fd6ecb0ca1d877f607e584
NLCaceres <NLCaceres21@Gmail.com>
2025-03-08 12:16:16 -0800

    Run `ruff` fixes with new `isort` import config on `tests/utility/test_datetime`
    - Correctly groups 3rd party and standard library dependencies
    - Also manually fixed lines that exceeded expected length of 90

--- tests/utility/test_datetime_helpers.py
+++ tests/utility/test_datetime_helpers.py
@@ -1,6 +1,9 @@
-from mlb_team_schedule.utility.datetime_helpers import dateToday, isDatetime, dateToStr, strToDatetime
+from mlb_team_schedule.utility.datetime_helpers import (
+    dateToday, dateToStr, isDatetime, strToDatetime,
+)
+
 import pytest
-from datetime import date, datetime, UTC
+from datetime import UTC, date, datetime
 
 
 def test_dateToday():
@@ -26,9 +29,9 @@ def test_isDatetime():
 
     #* WHEN using a string
     #* THEN isDatetime returns False
-    assert isDatetime('not a date') is False
+    assert isDatetime("not a date") is False
     #* THEN isDatetime returns False EVEN IF a string that could be formatted into a date
-    assert isDatetime('2021-03-01') is False
+    assert isDatetime("2021-03-01") is False
     
     #* WHEN using an int
     #* THEN isDatetime returns False
@@ -38,13 +41,13 @@ def test_isDatetime():
 def test_dateToStr():
     #* WHEN converting any date into a string using a specific format
     todays_date = dateToday()
-    todays_date_str = dateToStr(todays_date, '%Y-%m-%d')
-    [year_str, month_str, day_str] = todays_date_str.split('-')
-    #* THEN the date values will predictably appear in that format (allowing for easy splitting to grab for these tests)
-    assert int(year_str) == todays_date.year
+    todays_date_str = dateToStr(todays_date, "%Y-%m-%d")
+    [year_str, month_str, day_str] = todays_date_str.split("-")
+    #* THEN date values predictably appear in format
+    assert int(year_str) == todays_date.year # Also makes for easier assertions!
     assert int(month_str) == todays_date.month
     assert int(day_str) == todays_date.day
-    #? This would work in Javascript, BUT in Python, not all types are subscriptable, like lists or dicts
+    #? Not all Python types are subscriptable like list/dict or Javascript
     # date_elems = ['year', 'month', 'day']
     # for elem, prop in zip(date_str_elems, date_elems):
     #     assert elem == todays_date[prop]
@@ -52,8 +55,8 @@ def test_dateToStr():
 
 def test_strToDatetime():
     #* WHEN converting a string using a known format
-    date_str = '12/25/2010'
-    some_datetime = strToDatetime(date_str, '%m/%d/%Y')
+    date_str = "12/25/2010"
+    some_datetime = strToDatetime(date_str, "%m/%d/%Y")
     #* THEN the string is converted into a datetime properly
     assert some_datetime.year == 2010
     assert some_datetime.month == 12
@@ -61,6 +64,6 @@ def test_strToDatetime():
 
     #* WHEN converting a string using an incorrect/unknown format
     with pytest.raises(ValueError):
-        strToDatetime(date_str, '%Y/%m/%d')
-        #* THEN the string is NOT converted into a datetime properly and throws a ValueError due to unmatched format
-        #* BECAUSE strToDatetime functionally is an alias for datetime.strptime(some_date_str, format_str)
+        strToDatetime(date_str, "%Y/%m/%d")
+        #* THEN the string ISN'T converted into datetime, throwing ValueError
+        #* SINCE strToDateTime is basically a `datetime.strptime` alias
