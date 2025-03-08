23ed936b2d5ee4efb21781e7533adb97dd95302c
NLCaceres <NLCaceres21@Gmail.com>
2025-03-08 13:22:58 -0800

    Run `ruff` fixes + manually fix functions that exceed expected line-length
    - When function calls exceed the expected line-length, they must be split like
    other languages, BUT with Python, you must either wrap with parentheses OR
    use a trailing backslash.
    - Currently using trailing backslash to wrap function calls across multiple lines
    BUT `ruff` doesn't yet support E121 to E131, which check for line continuations
    like a trailing backslash or parentheses line wrap

--- tests/utility/test_database_helpers.py
+++ tests/utility/test_database_helpers.py
@@ -1,14 +1,19 @@
-from mlb_team_schedule.utility.database_helpers import saveToDb, finalizeDbUpdate
-from datetime import date
 from mlb_team_schedule import db
 from mlb_team_schedule.models import BaseballGame, BaseballTeam
+from mlb_team_schedule.utility.database_helpers import finalizeDbUpdate, saveToDb
+
+from datetime import date
+
 
 def test_saveToDb(app):
-    team1 = BaseballTeam(team_name='Foobar', city_name='Barfoo', team_logo='Foo', abbreviation='BF', wins=2, losses=1)
-    team2 = BaseballTeam(team_name='Buzz', city_name='Fizz', team_logo='Bar', abbreviation='FB', wins=1, losses=2)
-    game = BaseballGame(gameKey=1, date=date.today(), seriesGameNumber=1, seriesGameCount=3, home_team_id=1, away_team_id=2)
+    team1 = BaseballTeam(team_name="Foobar", city_name="Barfoo", team_logo="Foo",
+                         abbreviation="BF", wins=2, losses=1)
+    team2 = BaseballTeam(team_name="Buzz", city_name="Fizz", team_logo="Bar",
+                         abbreviation="FB", wins=1, losses=2)
+    game = BaseballGame(gameKey=1, date=date.today(), seriesGameNumber=1,
+                        seriesGameCount=3, home_team_id=1, away_team_id=2)
 
-    with app.app_context(): #? App Context required to interact with the DB (session.commit() & queries)
+    with app.app_context(): #? App Context needed to use the DB for queries + updates
         saveToDb(team1)
         saveToDb(team2)
         saveToDb(game)
@@ -19,18 +24,20 @@ def test_saveToDb(app):
 
 
 def test_finalizeDbUpdate(app):
-    team1 = BaseballTeam(team_name='Foobar', city_name='Barfoo', team_logo='Foo', abbreviation='BF', wins=2, losses=1)
+    team1 = BaseballTeam(team_name="Foobar", city_name="Barfoo", team_logo="Foo",
+                         abbreviation="BF", wins=2, losses=1)
 
     with app.app_context():
         saveToDb(team1)
         teams = db.session.scalars(db.select(BaseballTeam)).all()
         assert len(teams) == 1
-        assert teams[0].team_name == 'Foobar'
+        assert teams[0].team_name == "Foobar"
 
-        teams[0].team_name = 'Fizz'
+        teams[0].team_name = "Fizz"
         finalizeDbUpdate()
 
-        #? Must use .all() here to complete conversion into BaseballTeam List, not simply the iterable ScalarResult
-        teams_found = db.session.scalars(db.select(BaseballTeam).filter_by(team_name = 'Fizz')).all()
+        #? Need `all()` to convert into list[BaseballTeam] & not an iterable ScalarResult
+        teams_found = db.session \
+            .scalars(db.select(BaseballTeam).filter_by(team_name = "Fizz")).all()
         assert len(teams_found) == 1
-        assert teams_found[0].team_name == 'Fizz'
+        assert teams_found[0].team_name == "Fizz"
