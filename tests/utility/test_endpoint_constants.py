eb528d1b8b45e90de654c6f48decbc0c262718c4
NLCaceres <NLCaceres21@Gmail.com>
2025-03-09 00:49:24 -0800

    Run `ruff` fixes + manually fix URL strings that exceed line-length in `utility/test_endpoint`
    - `ruff` has an odd quirk where it ignores strings that exceed the expected line
    length IF they start with "http" i.e they're URLs.
    - Consequently, it is important to manually split these strings with parentheses
    across multiple lines in a way that is still readable and well-indented

--- tests/utility/test_endpoint_constants.py
+++ tests/utility/test_endpoint_constants.py
@@ -1,13 +1,17 @@
-from mlb_team_schedule.utility.endpoint_constants import SCHEDULE_ENDPOINT, LATEST_GAME_URL, BASE_MLB_LOGO_URL
+from mlb_team_schedule.utility.endpoint_constants import (
+    BASE_MLB_LOGO_URL, LATEST_GAME_URL, SCHEDULE_ENDPOINT,
+)
+
 import pytest
 
 
 def test_format_schedule_endpoint():
-    #? If the endpoint is used without a format() call, then the string KEEPS the placeholders
-    assert SCHEDULE_ENDPOINT == ('https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&hydrate=team'
-                                 ',game(promotions)&season={seasonYear}&startDate={startDate}&endDate={endDate}'
-                                 '&teamId={teamId}&gameType=R&scheduleTypes=games')
-    
+    #? If endpoint used w/out `format`, then string keeps placeholders
+    assert SCHEDULE_ENDPOINT == ("https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1"
+                                 "&hydrate=team,game(promotions)&season={seasonYear}"
+                                 "&startDate={startDate}&endDate={endDate}&teamId={teamId}"
+                                 "&gameType=R&scheduleTypes=games")
+
     #* Using format() without the keyname (i.e. seasonYear) throws an Exception
     with pytest.raises(KeyError):
         SCHEDULE_ENDPOINT.format(123)
@@ -16,21 +20,23 @@ def test_format_schedule_endpoint():
     with pytest.raises(KeyError):
         SCHEDULE_ENDPOINT.format(seasonYear = 123)
 
-    filled_schedule_endpoint = SCHEDULE_ENDPOINT.format(seasonYear = 123, startDate = 'Y-M-D', endDate = 'y-m-d', teamId=123)
-    assert filled_schedule_endpoint == ('https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&hydrate=team'
-                                             ',game(promotions)&season=123&startDate=Y-M-D&endDate=y-m-d'
-                                             '&teamId=123&gameType=R&scheduleTypes=games')
+    filled_schedule_endpoint = SCHEDULE_ENDPOINT \
+        .format(seasonYear = 123, startDate = "Y-M-D", endDate = "y-m-d", teamId=123)
+    assert filled_schedule_endpoint == ("https://statsapi.mlb.com/api/v1/schedule?lang=en"
+                                        "&sportId=1&hydrate=team,game(promotions)&season=123"
+                                        "&startDate=Y-M-D&endDate=y-m-d&teamId=123"
+                                        "&gameType=R&scheduleTypes=games")
 
 
 def test_format_latest_game_url():
-    assert LATEST_GAME_URL == 'https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&teamId={espnID}'
+    assert LATEST_GAME_URL == "https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&teamId={espnID}"
 
-    filled_latest_game_url = LATEST_GAME_URL.format(espnID = '123')
-    assert filled_latest_game_url == 'https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&teamId=123'
+    filled_latest_game_url = LATEST_GAME_URL.format(espnID = "123")
+    assert filled_latest_game_url == "https://statsapi.mlb.com/api/v1/schedule?lang=en&sportId=1&teamId=123"
 
 
 def test_format_base_mlb_logo_url():
-    assert BASE_MLB_LOGO_URL == 'https://www.mlbstatic.com/team-logos/{espnID}.svg'
+    assert BASE_MLB_LOGO_URL == "https://www.mlbstatic.com/team-logos/{espnID}.svg"
 
-    filled_mlb_logo_url = BASE_MLB_LOGO_URL.format(espnID = '456')
-    assert filled_mlb_logo_url == 'https://www.mlbstatic.com/team-logos/456.svg'
+    filled_mlb_logo_url = BASE_MLB_LOGO_URL.format(espnID = "456")
+    assert filled_mlb_logo_url == "https://www.mlbstatic.com/team-logos/456.svg"
