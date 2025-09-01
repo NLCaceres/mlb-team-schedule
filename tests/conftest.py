86021d189ef48019f3aee120ee3445a1d60c5346
NLCaceres <NLCaceres21@Gmail.com>
2025-09-01 15:08:59 -0700

    Put database creator in a "session" `pytest` fixture + restore Flask-Migrate `downgrade`
    - A "session" scoped `pytest` fixture ONLY is init ONCE per call of `python -m pytest`,
    assuming that the fixture is actually used, of course. This makes it ideal for
    init'ing the PostgreSQL test database, letting `Flask-Migrate` simply upgrade
    and downgrade the database as needed per test.
      - HOWEVER, the database creator does directly need to use the `os.environ` now
       instead of the app config to get the new test database url/name.
      - ALSO the `app` fixture STILL needs to `sa.engine.dispose()` its connections
      to ensure the database can be properly dropped later.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from mlb_team_schedule import create_app, db as sa
 import pytest
 import requests
 from dotenv import find_dotenv, load_dotenv
-from flask_migrate import upgrade
+from flask_migrate import downgrade, upgrade
 from sqlalchemy import create_engine, text
 from sqlalchemy.engine.url import make_url
 from sqlalchemy.exc import OperationalError
@@ -21,16 +21,9 @@ def load_env(): #? Find the right path to `/tests` & the env file
     env_file = find_dotenv(".env.tests")
     load_dotenv(env_file) #? Returns a bool indicating it successfully loaded the env file
 
-@pytest.fixture
-def app():
-    # db_fd, db_path = tempfile.mkstemp() #? db_path links to temp db file
-
-    os.environ["SECRET_KEY"] = f"{os.urandom(24)}"
-    app = create_app({ }) #* Passing an empty Dictionary ensures the TestConfig is used
-    app.config.from_mapping({"SECRET_KEY": os.environ["SECRET_KEY"]})
-
-    config: dict[str, str] = app.config.get_namespace("SQLALCHEMY_")
-    url = make_url(config["database_uri"])
+@pytest.fixture(scope="session")
+def database():
+    url = make_url(os.environ["TEST_DATABASE_URL"])
     engine = create_engine(url, isolation_level="AUTOCOMMIT")
     rootURL = url._replace(database="postgres")
     rootEngine = create_engine(rootURL, isolation_level="AUTOCOMMIT")
@@ -44,6 +37,21 @@ def app():
             statement = text(f"CREATE DATABASE {url.database} ENCODING 'utf8'")
             db.execute(statement)
 
+    yield
+
+    with rootEngine.begin() as db:
+        print(f"Dropping database = {url.database}")
+        statement = text(f"DROP DATABASE {url.database}")
+        db.execute(statement)
+
+@pytest.fixture
+def app(database):
+    # db_fd, db_path = tempfile.mkstemp() #? db_path links to temp db file
+
+    os.environ["SECRET_KEY"] = f"{os.urandom(24)}"
+    app = create_app({ }) #* Passing an empty Dictionary ensures the TestConfig is used
+    app.config.from_mapping({"SECRET_KEY": os.environ["SECRET_KEY"]})
+
     with app.app_context():
         upgrade() #? Setup test DB via migrations so can fill tables during tests
     # with app.test_client() as client:
@@ -54,12 +62,9 @@ def app():
     # os.close(db_fd) #? Only purpose of db_fd is to close file/db
     # os.unlink(db_path)
     with app.app_context():
-        # downgrade(revision="base") #? FULL reset DB, dropping migrations/tables to 'base'
-        sa.engine.dispose()
-        with rootEngine.begin() as db:
-            print(f"Dropping database = {url.database}")
-            statement = text(f"DROP DATABASE {url.database}")
-            db.execute(statement)
+        print("Downgrading and disposing")
+        downgrade(revision="base") #? FULL reset DB, dropping migrations/tables to 'base'
+        sa.engine.dispose() # Disconnect from DB. `upgrade` reforms connection later
 
 
 @pytest.fixture
